46715ba5614f0c4dcc98c8543bd5b0bdfaace872
ChocolateAceCream <nuodi@hotmail.com>
2024-09-01 08:53:34 -0700

    FEAT: add content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
         <div>
           <img src={logo} alt={companyName} className="w-16 h-16" />
         </div>
-        <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-4 h-12">
+        <div className="flex flex-wrap sm:gap-10 gap-2 items-center justify-center mt-4 h-12">
           {sections.map((section, index) => (
             <a
               key={`${section.name}-${index}`}
@@ -29,8 +29,8 @@ const About = () => {
         </div>
         <div className="flex items-center gap-x-8 mt-6 h-8">
           <a
-            aria-label="github"
-            href={socialMedia.github}
+            aria-label="facebook"
+            href={socialMedia.facebook} // Replace with your Facebook link
             target="_blank"
             rel="noreferrer"
           >
@@ -41,9 +41,10 @@ const About = () => {
               height="24"
               viewBox="0 0 24 24"
             >
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              <path d="M22.675 0h-21.35C.6 0 0 .6 0 1.35v21.301C0 23.4.6 24 1.325 24H12.82V14.706h-3.138v-3.65h3.138V8.413c0-3.1 1.893-4.787 4.655-4.787 1.325 0 2.466.099 2.797.143v3.24l-1.918.001c-1.504 0-1.794.715-1.794 1.762v2.31h3.587l-.467 3.65h-3.12V24h6.116C23.4 24 24 23.4 24 22.675V1.35C24 .6 23.4 0 22.675 0z" />
             </svg>
           </a>
+
           <a
             aria-label="linkedin"
             href={socialMedia.linkedin}
