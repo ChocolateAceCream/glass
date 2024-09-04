5d57c63c16e0d86044a066d7dc5a6b55c5e86cd7
ChocolateAceCream <nuodi@hotmail.com>
2024-09-03 22:14:04 -0700

    FEAT: remove price estimate

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,60 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { TextField, InputAdornment, MenuItem, Link } from '@mui/material';
+import { Link } from '@mui/material';
 
-const glassTypeOptions = [
-  {
-    value: 'Clear',
-    label: 'Clear',
-  },
-  {
-    value: 'Texture',
-    label: 'Texture',
-  },
-  {
-    value: 'Mirror',
-    label: 'Mirror',
-  },
-];
+// const glassTypeOptions = [
+//   {
+//     value: "Clear",
+//     label: "Clear",
+//   },
+//   {
+//     value: "Texture",
+//     label: "Texture",
+//   },
+//   {
+//     value: "Mirror",
+//     label: "Mirror",
+//   },
+// ];
 
 const Pricing = () => {
-  const [height, setHeight] = useState<number>(0);
-  const [width, setWidth] = useState<number>(0);
-  const [glassType, setGlassType] = useState<string>('Tempered');
-  const [price, setPrice] = useState<number>(0);
+  // const [height, setHeight] = useState<number>(0);
+  // const [width, setWidth] = useState<number>(0);
+  // const [glassType, setGlassType] = useState<string>("Tempered");
+  // const [price, setPrice] = useState<number>(0);
 
-  const calculatePrice = (h: number, w: number, t: string) => {
-    let newPrice = 0;
-    if (h > 0 && w > 0) {
-      newPrice = h * w;
-      if (t === 'Clear') {
-        newPrice *= 2;
-      } else {
-        newPrice *= 4;
-      }
-    }
-    setPrice(newPrice);
-  };
+  // const calculatePrice = (h: number, w: number, t: string) => {
+  //   let newPrice = 0;
+  //   if (h > 0 && w > 0) {
+  //     newPrice = h * w;
+  //     if (t === "Clear") {
+  //       newPrice *= 2;
+  //     } else {
+  //       newPrice *= 4;
+  //     }
+  //   }
+  //   setPrice(newPrice);
+  // };
 
-  const handleHeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newHeight = parseFloat(event.target.value) || 0; // Convert to number
-    setHeight(newHeight);
-    calculatePrice(newHeight, width, glassType);
-  };
+  // const handleHeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const newHeight = parseFloat(event.target.value) || 0; // Convert to number
+  //   setHeight(newHeight);
+  //   calculatePrice(newHeight, width, glassType);
+  // };
 
-  const handleWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newWidth = parseFloat(event.target.value) || 0; // Convert to number
-    setWidth(newWidth);
-    calculatePrice(height, newWidth, glassType);
-  };
+  // const handleWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const newWidth = parseFloat(event.target.value) || 0; // Convert to number
+  //   setWidth(newWidth);
+  //   calculatePrice(height, newWidth, glassType);
+  // };
 
-  const handleGlassTypeChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const newGlassType = event.target.value;
-    setGlassType(newGlassType);
-    calculatePrice(height, width, newGlassType);
-  };
+  // const handleGlassTypeChange = (
+  //   event: React.ChangeEvent<HTMLInputElement>
+  // ) => {
+  //   const newGlassType = event.target.value;
+  //   setGlassType(newGlassType);
+  //   calculatePrice(height, width, newGlassType);
+  // };
 
   return (
     <section className={`bg-background py-8`} id="pricing">
@@ -62,102 +62,30 @@ const Pricing = () => {
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
         >
-          Quick Estimate
+          Get A Quote
         </h1>
         <div className={`w-full`}>
           <div
             className={`h-1 mx-auto bg-primary w-64 opacity-25 my-0 py-0 rounded-t`}
           ></div>
         </div>
+
         <div
           className={`flex flex-col sm:flex-row justify-center my-12 sm:my-4`}
         >
           <div
             className={`flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-background mt-4`}
           >
-            <div
-              className={`flex-1 bg-background text-gray-600 rounded-t rounded-b-none overflow-hidden shadow`}
-            >
-              <div className={`p-4 text-3xl font-bold text-center`}>
-                <TextField
-                  label="Width"
-                  id="outlined-start-adornment"
-                  slotProps={{
-                    input: {
-                      startAdornment: (
-                        <InputAdornment position="start">Inch</InputAdornment>
-                      ),
-                    },
-                    htmlInput: {
-                      min: 1, // Minimum value to ensure only positive numbers
-                      step: 1, // Step value to ensure only integer numbers
-                      inputMode: `"numeric"`, // Mobile-friendly numeric keyboard
-                    },
-                  }}
-                  type="number"
-                  style={{ width: '90%' }}
-                  onChange={handleWidthChange}
-                />
-              </div>
-              <div className={`p-4 text-3xl font-bold text-center`}>
-                <TextField
-                  label="Height"
-                  id="outlined-start-adornment"
-                  slotProps={{
-                    input: {
-                      startAdornment: (
-                        <InputAdornment position="start">Inch</InputAdornment>
-                      ),
-                    },
-                    htmlInput: {
-                      min: 1, // Minimum value to ensure only positive numbers
-                      step: 1, // Step value to ensure only integer numbers
-                      inputMode: `'numeric'`, // Mobile-friendly numeric keyboard
-                    },
-                  }}
-                  type="number"
-                  style={{ width: '90%' }}
-                  onChange={handleHeightChange}
-                />
-              </div>
-              <div className={`p-4 text-3xl font-bold text-center`}>
-                <TextField
-                  id="outlined-select-glass-type"
-                  select
-                  label="Glass Type"
-                  defaultValue="Tempered"
-                  helperText="Please select glass type"
-                  style={{ width: '90%' }}
-                  onChange={handleGlassTypeChange}
-                >
-                  {glassTypeOptions.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </div>
-            </div>
-            <div
-              className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6`}
-            >
-              <div
-                className={`w-full pt-6 text-3xl text-gray-600 font-bold text-center`}
+            <div>
+              To get a quote, please fill the&nbsp;
+              <Link
+                href="/assets/documents/quote_form.pdf"
+                target="_blank"
+                rel="noopener"
               >
-                Estimated Cost: ${price}
-                {/* <span className={`text-base`}> {firstPlan?.priceDetails}</span> */}
-              </div>
-              <div>
-                For detailed quote, please fill the&nbsp;
-                <Link
-                  href="/assets/documents/quote_form.pdf"
-                  target="_blank"
-                  rel="noopener"
-                >
-                  form
-                </Link>
-                &nbsp; and email back to us
-              </div>
+                form
+              </Link>
+              &nbsp; and email back to seattle@unikoogroup.com
             </div>
           </div>
           {/* <div
