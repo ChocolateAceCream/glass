62f01813313c95e8f05fd1fe1a8a603b81086388
ChocolateAceCream <nuodi@hotmail.com>
2024-09-04 09:28:45 -0700

    feat: add live chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,41 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Script
+      id="zoho-salesiq"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+              var $zoho = $zoho || {};
+              $zoho.salesiq = $zoho.salesiq || {
+                widgetcode: "735d5da750431b6b984546c4b0c692feaeefb8a8421e7d4353878dfcb3dcd9aa",
+                values: {},
+                ready: function () {}
+              };
+              (function() {
+                var d = document;
+                var s = d.createElement("script");
+                s.type = "text/javascript";
+                s.id = "zsiqscript";
+                s.defer = true;
+                s.src = "https://salesiq.zohocloud.ca/widget";
+                var t = d.getElementsByTagName("script")[0];
+                t.parentNode.insertBefore(s, t);
+
+                // Create the widget container div safely
+                var widgetDiv = d.createElement("div");
+                widgetDiv.id = "zsiqwidget";
+                d.body.appendChild(widgetDiv);
+              })();
+            `,
+      }}
+    />
+  </>
 );
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Script from 'next/script';
+
 import Analytics from '../components/Analytics';
 import Canvas from '../components/Canvas';
 import Catalog from '../components/Catalog';
@@ -14,6 +16,35 @@ import Product from '../components/Product';
 const App = () => {
   return (
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
+      <Script
+        id="zoho-salesiq"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              var $zoho = $zoho || {};
+              $zoho.salesiq = $zoho.salesiq || {
+                widgetcode: "735d5da750431b6b984546c4b0c692feaeefb8a8421e7d4353878dfcb3dcd9aa",
+                values: {},
+                ready: function () {}
+              };
+              (function() {
+                var d = document;
+                var s = d.createElement("script");
+                s.type = "text/javascript";
+                s.id = "zsiqscript";
+                s.defer = true;
+                s.src = "https://salesiq.zohocloud.ca/widget";
+                var t = d.getElementsByTagName("script")[0];
+                t.parentNode.insertBefore(s, t);
+
+                // Create the widget container div safely
+                var widgetDiv = d.createElement("div");
+                widgetDiv.id = "zsiqwidget";
+                d.body.appendChild(widgetDiv);
+              })();
+            `,
+        }}
+      />
       <div className={`relative bg-background`}>
         <div className="max-w-7xl mx-auto">
           <div
